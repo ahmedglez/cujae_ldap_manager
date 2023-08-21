63e476253f4be947664fc0a88e8aa2414cda4933
Ahmedglez <ahmediglez@gmail.com>
2023-08-21 11:03:26 -0400

    Validate ldap connection error

--- src/connections/LDAP_client.js
+++ src/connections/LDAP_client.js
@@ -2,18 +2,35 @@ const ldap = require('ldapjs')
 const config = require('../config/config')
 var assert = require('assert')
 
-const client = ldap.createClient({
-  url: [`${config.ldap.url}:${config.ldap.port}`],
-  connectTimeout: 60000,
-  reconnect: true,
-})
+const initializeLDAPConnection = () => {
+  try {
+    const client = ldap.createClient({
+      url: [`${config.ldap.url}:${config.ldap.port}`],
+      connectTimeout: 60000,
+      reconnect: true,
+    })
 
-client.on('connectError', (err) => {
-  console.log("ERROR")
-})
+    client.on('connectError', (err) => {
+      console.log('LDAP Connection Error:', err.message)
+    })
 
-client.bind(config.ldap.admin.username, config.ldap.admin.password, (err) => {
-  assert.ifError(err)
-})
+    client.bind(
+      config.ldap.admin.username,
+      config.ldap.admin.password,
+      (err) => {
+        if (err) {
+          console.error('LDAP Bind Error:', err.message)
+        } else {
+          console.log('LDAP Connection Successful')
+        }
+      }
+    )
 
-module.exports = client
+    return client
+  } catch (error) {
+    console.error('Error initializing LDAP connection:', error.message)
+    return null // Return null or a default client if the connection setup fails
+  }
+}
+
+module.exports = initializeLDAPConnection
