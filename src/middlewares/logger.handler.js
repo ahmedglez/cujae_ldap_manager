a9dd13d15db01228d278a776c2eb8504c19d7e61
Ahmedglez <ahmediglez@gmail.com>
2023-08-21 10:52:32 -0400

    protect database connection in case of error

--- src/middlewares/logger.handler.js
+++ src/middlewares/logger.handler.js
@@ -20,32 +20,42 @@ const logger = winston.createLogger({
   ],
 })
 
-const addLoggerMiddleware = (app) => {
-  morgan.token('user', (req) => {
-    return req.user ? req.user.uid : 'anonymous'
-  })
-  app.use(
-    morgan(function (tokens, req, res) {
-      const log = {
-        method: tokens.method(req, res),
-        url: tokens.url(req, res),
-        status: tokens.status(req, res),
-        content_length: tokens.res(req, res, 'content-length'),
-        response_time: tokens['response-time'](req, res),
-        user: req.user === undefined ? 'anonimus' : req.user.uid,
-      }
-      logger.info({ ...log })
+morgan.token('user', (req) => {
+  return req.user ? req.user.uid : 'anonymous'
+})
 
-      return [
-        `method:${log.method}`,
-        `url:${log.url}`,
-        `status:${log.status}`,
-        `content-lenght:${log.content_length}`,
-        `response-time:${log.response_time}ms`,
-        `user_uid:${log.user}`,
-      ].join(' ')
-    })
-  )
+const logFormat = (tokens, req, res) => {
+  const log = {
+    method: tokens.method(req, res),
+    url: tokens.url(req, res),
+    status: tokens.status(req, res),
+    content_length: tokens.res(req, res, 'content-length'),
+    response_time: tokens['response-time'](req, res),
+    user: req.user === undefined ? 'anonymous' : req.user.uid,
+  }
+
+  logger.info({ ...log })
+
+  return [
+    `method:${log.method}`,
+    `url:${log.url}`,
+    `status:${log.status}`,
+    `content-length:${log.content_length}`,
+    `response-time:${log.response_time}ms`,
+    `user_uid:${log.user}`,
+  ].join(' ')
+}
+
+const addLoggerMiddleware = (app) => {
+  try {
+    // Attempt to establish the database connection and configure logger
+    winston.add(logger.transports[0])
+
+    // Register the morgan middleware
+    app.use(morgan(logFormat))
+  } catch (error) {
+    console.error('Error connecting to the database:', error.message)
+  }
 }
 
 module.exports = addLoggerMiddleware
