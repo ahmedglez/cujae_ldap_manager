262a3e0d3268daaa9bf91246f31dbb0a2a90ec1d
Ahmedglez <ahmediglez@gmail.com>
2023-08-30 18:33:15 -0400

    add userType filter endpoint

---
+++ src/constants/userTypes.js
@@ -0,0 +1,8 @@
+const userTypes = ['Estudiante', 'Trabajador Docente', 'Trabajador']
+
+const user_types_query = ['student', 'employee', 'docent_employee']
+
+module.exports = {
+  userTypes,
+  user_types_query,
+}
--- src/helpers/convertQueryToFilter.js
+++ src/helpers/convertQueryToFilter.js
@@ -1,4 +1,12 @@
 /* thank you chatgpt */
+const { userTypes } = require('@src/constants/userTypes')
+
+const userTypeFilters = {
+  student: userTypes[0],
+  employee: userTypes[1],
+  docent_employee: userTypes[2],
+}
+
 const createLdapFilterFromQuery = (query) => {
   const filters = []
 
@@ -7,27 +15,31 @@ const createLdapFilterFromQuery = (query) => {
   }
 
   if (query.uid) {
-    filters.push(`uid=${query.uid}`)
+    filters.push(`(uid=${query.uid})`)
   }
 
   if (query.cn) {
-    filters.push(`cn=${query.cn}`)
+    filters.push(`(cn=${query.cn})`)
   }
 
   if (query.username) {
-    filters.push(`uid=${query.username}`)
+    filters.push(`(uid=${query.username})`)
   }
 
   if (query.ci) {
-    filters.push(`ci=${query.ci}`)
+    filters.push(`(ci=${query.ci})`)
   }
 
   if (query.email) {
-    filters.push(`maildrop=${query.email}`)
+    filters.push(`(maildrop=${query.email})`)
+  }
+
+  if (query.userType && userTypeFilters[query.userType]) {
+    filters.push(`(userType=${userTypeFilters[query.userType]})`)
   }
 
   // Combine multiple filters using logical AND
-  const ldapFilter = `(${filters.join('')})`
+  const ldapFilter = `${filters.join('')}`
 
   return ldapFilter
 }
