2cf12f360634fb85f013e2f6f9ee1815ebeeb4c6
Ahmedglez <ahmediglez@gmail.com>
2023-09-11 19:28:05 -0400

    add userAndRecoveryCode schema

---
+++ src/modules/forgot-password/services/restore-password.service.js
@@ -0,0 +1,96 @@
+const { config } = require('@src/config/config')
+const nodemailer = require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport')
+const handlebars = require('handlebars')
+const fs = require('fs')
+const userServices = require('@src/services/user.services')()
+const path = require('path')
+const UserAndRecoveryCode = require('../schemas/user&recoveryCode.schema')
+const { generateRandomSixDigitNumber, readHTMLFile } = require('../utils/index')
+
+const generateRecoveryCode = async (user, expiration) => {
+  try {
+    let userInDB = await UserAndRecoveryCode.findOne({
+      username: user.uid,
+    })
+
+    const code = generateRandomSixDigitNumber()
+
+    if (!userInDB) {
+      // If the user doesn't exist, create a new user
+      const newUserInDB = new UserAndRecoveryCode({
+        username: user.uid, // Adjust the field name as needed
+        // Set other user fields as needed (e.g., name, email, password)
+        // ...
+        email: user.maildrop,
+        recoveryCode: {
+          code: code,
+          expiresAt: expiration,
+        },
+      })
+      newUserInDB.save()
+    } else {
+      // If the user exists, update the recovery code
+      userInDB.recoveryCode.code = code
+      userInDB.recoveryCode.expiresAt = expiration
+      await userInDB.save()
+    }
+  } catch (error) {
+    throw error
+  }
+}
+
+const sendRecoveryPasswordEmailTo = async (user) => {
+  try {
+    const name = user.name
+    const email = user.maildrop
+
+    const transporter = nodemailer.createTransport(
+      smtpTransport({
+        service: process.env.EMAIL_SERVICE,
+        host: process.env.EMAIL_HOST,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      })
+    )
+
+    readHTMLFile(
+      path.join(__dirname, '../templates/reset-password-email.html'),
+      function (err, html) {
+        if (err) {
+          console.log('error reading file', err)
+          return
+        }
+
+        const template = handlebars.compile(html)
+        const replacements = {
+          name,
+        }
+        const htmlToSend = template(replacements)
+
+        const mailOptions = {
+          from: process.env.EMAIL_USER,
+          to: email,
+          subject: 'Reestablecimiento de Contrasena',
+          html: htmlToSend,
+        }
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error)
+          } else {
+            console.log('Email sent: ' + info.response)
+          }
+        })
+      }
+    )
+  } catch (error) {
+    console.error(error)
+    throw new Error('Error on sending email')
+  }
+}
+
+module.exports = { generateRecoveryCode, sendRecoveryPasswordEmailTo }
