d591d726e24a3684155d231f5f04e113bf77b15b
Ahmedglez <ahmediglez@gmail.com>
2023-09-16 18:04:36 -0400

    add try catch to ldap client

--- src/connections/LDAP_client.js
+++ src/connections/LDAP_client.js
@@ -1,23 +1,34 @@
 const ldap = require('ldapjs')
 const config = require('../config/config')
-var assert = require('assert')
+const assert = require('assert')
 
-const client = ldap.createClient({
-  url: [`${config.ldap.url}:${config.ldap.port}`],
-  connectTimeout: 60000,
-  reconnect: true,
-})
+try {
+  const client = ldap.createClient({
+    url: [`${config.ldap.url}:${config.ldap.port}`],
+    connectTimeout: 60000,
+    reconnect: true,
+  })
 
-client.on('connectError', (err) => {
-  console.log('Error trying to connect to LDAP', err)
-})
+  client.on('connectError', (err) => {
+    console.log('Error trying to connect to LDAP', err)
+  })
 
-client.bind(config.ldap.admin.username, config.ldap.admin.password, (err) => {
-  if (err) {
-    assert.ifError(err)
-  } else {
-    console.log('Binded to LDAP')
-  }
-})
+  client.bind(config.ldap.admin.username, config.ldap.admin.password, (err) => {
+    try {
+      if (err) {
+        console.error('Error binding to LDAP:', err)
+        // Handle the error gracefully here, e.g., log it or perform other actions
+      } else {
+        console.log('Binded to LDAP')
+      }
+    } catch (bindError) {
+      console.error('Error during LDAP binding:', bindError)
+      // Handle the binding error gracefully here
+    }
+  })
 
-module.exports = client
+  module.exports = client
+} catch (clientError) {
+  console.error('Error creating LDAP client:', clientError)
+  // Handle the LDAP client creation error gracefully here
+}
